349624ca30ad6b607403c6fdbb806bdb4bf5ab22
Charles B Johnson <mail@cbjohnson.info>
2013-08-15 04:43:57 -0400

    chore: update gitignore, change title helper slightly to be more succinct

--- helpers/title_helpers.rb
+++ helpers/title_helpers.rb
@@ -1,21 +1,17 @@
 module TitleHelpers
 
   def full_title
-    title = ""
-    if current_page.data.title
-      title << "#{current_page.data.title} | blog | "
+    title = if current_page.data.title
+      "#{current_page.data.title} | blog"
     elsif current_page.data.section
-      title << "#{current_page.data.section} | "
+      "#{current_page.data.section}"
     end
-    title << data.site.name
+
+    title << " | #{data.site.name}"
   end
 
   def section_title
-    if current_page.data.title
-      'blog'
-    else
-      current_page.data.section
-    end
+    current_page.data.title ? 'blog' : current_page.data.section
   end
 
 end
