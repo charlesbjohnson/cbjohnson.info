5d27d655df0bba97867d23f7d150893fc65c0399
Charles B Johnson <mail@cbjohnson.info>
2013-08-16 19:43:44 -0400

    chore: fix issue with nav highlighting when reading a blog post. simplify colors based on feedback.

--- helpers/title_helpers.rb
+++ helpers/title_helpers.rb
@@ -14,4 +14,8 @@ module TitleHelpers
     current_page.data.title ? 'blog' : current_page.data.section
   end
 
+  def active_section(route)
+    is_blog_section = route == data.site.routes.blog && is_blog_article?
+    current_page.url == route || is_blog_section
+  end
 end
