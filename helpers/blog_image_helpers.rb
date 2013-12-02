f8181d880b64382c87fe60b2834b652954c2f408
Charles B Johnson <mail@cbjohnson.info>
2013-12-02 18:10:50 -0500

    chore: attempt to fix blog images yet again. bail on relative assets, resort to absolute uri's for images.

--- helpers/blog_image_helpers.rb
+++ helpers/blog_image_helpers.rb
@@ -3,13 +3,6 @@ require 'find'
 module BlogImageHelpers
 
   def blog_image(image)
-    return find_blog_image(image) if current_article.nil? || environment == :development
-    image
-  end
-
-  private
-
-  def find_blog_image(image)
     blog_files = Find.find(File.expand_path('blog', 'source')).to_a
     blog_files.grep(/#{image}/).first[/\/blog.+/]
   end
